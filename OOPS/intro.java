2b731ee7b5a474b2eaa726368969e097aab006bf
Aryan <Aryanchaudhary2629@gmail.com>
2025-05-04 11:27:29 +0530

    Updated Code

---
+++ OOPS/intro.java
@@ -0,0 +1,15 @@
+package OOPS;
+
+class Animal{
+
+    void AnimalName(){
+        System.out.println("I am Tiger");
+    }
+}
+
+public class intro {
+    public static void main(String[] args) {
+        Animal an = new Animal();
+        an.AnimalName();
+    }
+}
