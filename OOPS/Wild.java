419b85a2a38f77dc423d7087dbd53c7b1c836362
Aryan <Aryanchaudhary2629@gmail.com>
2025-05-04 11:10:06 +0530

    Updated Code

---
+++ OOPS/Wild.java
@@ -0,0 +1,13 @@
+class Animal{  
+    
+    void AnimalName(){
+        System.out.println("I am Dog");
+    }
+}
+
+public class Wild{
+    public static void main(String args[]){
+        Animal d = new Animal();
+        d.AnimalName();
+    }
+}
